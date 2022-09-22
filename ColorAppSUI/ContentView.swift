8b93ebe9578b72d7c947835091bcd860a38895ee
gaidaasd00 <swiftgaida@list.ru>
2022-09-22 18:25:00 +0300

    1. added new branch with the keyboard settings

--- ColorAppSUI/ContentView.swift
+++ ColorAppSUI/ContentView.swift
@@ -12,35 +12,75 @@ struct ContentView: View {
     @State private var green = Double.random(in: 0...255).rounded()
     @State private var blue = Double.random(in: 0...255).rounded()
 
-    @FocusState private var inInputActiv: Bool
+    @FocusState private var focusField: Field?
     var body: some View {
         ZStack {
             Color(red: 0, green: 0.3332323, blue: 0.13434223425)
                 .ignoresSafeArea()
                 .onTapGesture {
-                    inInputActiv = false
+                    focusField = nil
                 }
             VStack(spacing: 40) {
                 ColorView(red: red, green: green, blue: blue)
                 
                 VStack {
                     ColorSliderView(value: $red, color: .red)
+                        .focused($focusField, equals: .red)
                     ColorSliderView(value: $green, color: .green)
+                        .focused($focusField, equals: .green)
                     ColorSliderView(value: $blue, color: .blue)
+                        .focused($focusField, equals: .blue)
                 }
                 .frame(height: 150)
-                .focused($inInputActiv)
                 .toolbar {
                     ToolbarItemGroup(placement: .keyboard) {
+                        Button(action: preViewsField) {
+                            Image(systemName: "chevron.up")
+                        }
+                        Button(action: nextField) {
+                            Image(systemName: "chevron.down")
+                        }
                         Spacer()
                         Button("Done") {
-                            inInputActiv = false
+                            focusField = nil
                         }
                     }
                 }
                 Spacer()
             }
             .padding()
+        }
+    }
+}
+
+extension ContentView {
+    private enum Field {
+        case red
+        case green
+        case blue
+    }
+    private func nextField() {
+        switch focusField {
+        case .red:
+            focusField = .blue
+        case .green:
+            focusField = .red
+        case .blue:
+            focusField = .green
+        case .none:
+            focusField = nil
+        }
+    }
+        private func preViewsField() {
+            switch focusField {
+            case .red:
+                focusField = .blue
+            case .green:
+                focusField = .red
+            case .blue:
+                focusField = .green
+            case .none:
+                focusField = nil
         }
     }
 }
