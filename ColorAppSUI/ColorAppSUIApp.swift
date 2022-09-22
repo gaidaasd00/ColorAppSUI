05da4185b80d65d70f96eaf4c3b1c28093b62c59
gaidaasd00 <swiftgaida@list.ru>
2022-09-22 17:04:16 +0300

    Initial Commit

---
+++ ColorAppSUI/ColorAppSUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ColorAppSUIApp.swift
+//  ColorAppSUI
+//
+//  Created by Алексей Гайдуков on 22.09.2022.
+//
+
+import SwiftUI
+
+@main
+struct ColorAppSUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
